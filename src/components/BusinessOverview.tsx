371be2d7ad3c21001643b78fe8db4b0adbcf51b3
Elio824 <imtaran824@gmail.com>
2025-09-23 18:21:26 +0530

    Updated BusinessOverview.tsx

--- src/components/BusinessOverview.tsx
+++ src/components/BusinessOverview.tsx
@@ -1463,7 +1463,6 @@ export const BusinessOverview = () => {
           </div>
 
           
-      </div>
       
       {/* Footer */}
       <div className="absolute bottom-8 left-1/2 transform -translate-x-1/2">
