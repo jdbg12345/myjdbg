608e1cac003c2ab9779dfd8e208056e11bd9a12d
imdouble824 <imdouble824@outlook.com>
2025-10-01 18:06:29 +0800

    Updated FKSystemOverview.tsx

--- src/components/FKSystemOverview.tsx
+++ src/components/FKSystemOverview.tsx
@@ -811,7 +811,6 @@ export const FKSystemOverview = () => {
     </div>
       
     {/* Footer */}
-    {/* Footer */}
   <div className="absolute bottom-8 left-1/2 transform -translate-x-1/2">
     <div className="text-gray-400 text-sm">FKZX</div>
   </div>
