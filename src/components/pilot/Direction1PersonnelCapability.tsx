9e078c60c7451ba1071e65416aa2abe481246fb1
zsslufe9 <zsslufe9@outlook.com>
2025-10-03 23:55:13 +0800

    Updated Direction1PersonnelCapability.tsx

--- src/components/pilot/Direction1PersonnelCapability.tsx
+++ src/components/pilot/Direction1PersonnelCapability.tsx
@@ -363,7 +363,7 @@ export const Direction1PersonnelCapability = () => {
                     {[
                       { name: '外包一审', before: 66423, after: 24511 },
                       { name: '远程一审', before: 82773, after: 29431 },
-                      { name: '总部一审', before: 25161, after: 135259 }
+                      { name: '总部一审', before: 25161, after: 135259 },
                       { name: '总部二审', before: 14472, after: 145628 }
                     ].map((item, index) => (
                       <div key={index} className="text-center" style={{ width: '160px' }}>
