9d2c175418e2017ddc0ddae618732ad9804fc4bf
zsslufe9 <zsslufe9@outlook.com>
2025-10-05 05:46:13 +0800

    Updated CoverPage.tsx

--- src/components/CoverPage.tsx
+++ src/components/CoverPage.tsx
@@ -8,7 +8,6 @@ export const CoverPage = () => {
       
       {/* Footer */}
       <div className="absolute bottom-8 left-1/2 transform -translate-x-1/2">
-        <div className="text-gray-400 text-sm">FKZX Q3 REPORT</div>
       </div>
     </div>
   );
